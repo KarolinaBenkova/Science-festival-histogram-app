014faa3a00f5726f53e45b6938192aa1a007e612
Karolina <s1989816@ed.ac.uk>
2023-03-21 03:51:50 +0000

    added conversion ft,in to cm, ignored zero heights in code

--- shiny_histogram.R
+++ shiny_histogram.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(magrittr)
 library(readxl)
+# Modified from https://shiny.rstudio.com/tutorial/written-tutorial/lesson1/
 
 ui <- shinyServer(fluidPage(
   titlePanel("Heights of Edinburgh Science Festival participants"),
@@ -26,12 +27,14 @@ server <- shinyServer(function(input, output, session){
   }
   output$adultPlot <- renderPlot({
     print("Render")
-    invalidateLater(5000, session)
+    print(Sys.time())
+    invalidateLater(5000, session) # refresh every 5 sec
     update_adult()
-    print(adult_heights)
+    print(adult_heights$heights[adult_heights$heights>0])
     
     x    <- adult_heights$heights
-    x    <- na.omit(x)
+    x    <- na.omit(x) # ignore invalid entries
+    x    <- x[x>0] # ignore zero heights
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     hist(x, breaks = bins, col = "#75AADB", border = "black",
          xlab = "Height [cm]",
